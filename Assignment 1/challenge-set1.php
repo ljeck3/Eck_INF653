385092408ca77bc4c2d0a9f47eabefbbe0a866ef
lj-1003 <levijustin24@gmail.com>
2026-01-28 13:25:48 -0600

    Challenge Set 1 (From 1/26/26)

---
+++ Assignment 1/challenge-set1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<body>
+<?php
+    #challenge1
+    $name = "Levi";
+    $age = "22";
+    $favorite_color = "Blue";
+    echo "My name is $name. I am $age years old, and my favorite color is $favorite_color.";
+?>
+<br>
+<?php
+    #challenge2.1
+    echo "\"He said, \"PHP is fun!\" and left.\"";
+?>
+<br>
+<?php
+    #challenge2.2
+    echo "First line \nSecond line";
+?>
+<br>
+<?php
+    #challenge3
+    $greeting = 'Hello';
+    echo "Welcome to the PHP world! ";
+    echo "Your age is " . $age;
+?>
+<br>
+<?php
+    #challenge4
+    echo "Welcome to PHP! ";
+    $name= "John";
+    echo "Hello, $name";
+?>
+<br>
+<?php
+    #challenge5
+    
+    /* This challenge works on adding comments.
+    Different styles will be demonstrated */
+
+    $price = 50; // sets the price variable
+    $discount = 10; // sets the discount variable
+    $finalPrice = $price - $discount; //does the math
+    echo "Total price: $" . $finalPrice;
+?>
+</body>
+</html>
