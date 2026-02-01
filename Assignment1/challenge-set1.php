f7df15e5a1650dc307a077fe9f4741ebf2979c80
lj-1003 <levijustin24@gmail.com>
2026-02-01 14:55:59 -0600

    minor corrections

--- Assignment1/challenge-set1.php
+++ Assignment1/challenge-set1.php
@@ -5,31 +5,27 @@
     $name = "Levi";
     $age = "22";
     $favorite_color = "Blue";
-    echo "My name is $name. I am $age years old, and my favorite color is $favorite_color.";
+    echo "My name is $name, I am $age years old, and my favorite color is $favorite_color.". "<br>";
 ?>
 <br>
 <?php
-    #challenge2.1
-    echo "\"He said, \"PHP is fun!\" and left.\"";
-?>
-<br>
-<?php
-    #challenge2.2
-    echo "First line \nSecond line";
+    #challenge2
+    echo "\"He said, \"PHP is fun!\" and left.\"". "<br>";
+    echo "First line \n<br>Second line". "<br>"; //added <br> so it renders in browser.
 ?>
 <br>
 <?php
     #challenge3
-    $greeting = 'Hello';
-    echo "Welcome to the PHP world! ";
-    echo "Your age is " . $age;
+    $greeting = 'Welcome to the PHP world!';
+    echo $greeting . "<br>";
+    echo "\nYour age is " . $age. ".<br>";
 ?>
 <br>
 <?php
     #challenge4
     echo "Welcome to PHP! ";
     $name= "John";
-    echo "Hello, $name";
+    echo "Hello, " . $name . ".<br>";
 ?>
 <br>
 <?php
