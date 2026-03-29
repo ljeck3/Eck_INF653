c3c4d651a58f95f7071388a0ebcdf9c59a76b633
lj-1003 <levijustin24@gmail.com>
2026-03-29 15:37:35 -0500

    Vehicles appear on public home page.

--- zippyusedautos/view/vehicles.php
+++ zippyusedautos/view/vehicles.php
@@ -1,4 +1,23 @@
-
 <section class="">
-    <h2>Vehicles</h2>
+    <h1>Vehicles</h1>
+    <table>
+    <tr>
+        <th>Year</th>
+        <th>Make</th>
+        <th>Model</th>
+        <th>Type</th>
+        <th>Class</th>
+        <th>Price</th>
+    </tr>
+    <?php foreach($vehicles as $vehicle): ?>
+        <tr>
+            <td><?= $vehicle['year'] ?></td>
+            <td><?= $vehicle['make_name'] ?></td>
+            <td><?= $vehicle['model'] ?></td>
+            <td><?= $vehicle['type_name'] ?></td>
+            <td><?= $vehicle['class_name'] ?></td>
+            <td>$<?= $vehicle['price'] ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
 </section>
